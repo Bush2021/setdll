54821c62b2a14d0dfff8521ede34ae0963054213
Bush2021 <79072750+Bush2021@users.noreply.github.com>
2025-04-09 19:39:45 -0400

    feat: add YY-Thunks to support down to Windows XP
    Close #3.

--- YY-Thunks.lua
+++ YY-Thunks.lua
@@ -1,31 +1,30 @@
-target("VC-LTL-5")
+target("YY-Thunks")
     set_kind("phony")
     before_build("windows", function (target)
         local function find_in_file()
             for _, dir in ipairs(os.dirs("$(projectdir)/*")) do
                 name = dir:match(".*\\(.*)")
-                if name:find("VC%-LTL") then
-                    return dir .. [[\build\native\]]
+                if name:find("YY%-Thunks") then
+                    return dir .. [[\]]
                 end
             end
         end
         local function find_in_reg()
-            return vformat("$(reg HKEY_CURRENT_USER\\Code\\VC-LTL;Root)")
+            return vformat("$(reg HKEY_CURRENT_USER\\Code\\YY-Thunks;Root)")
         end
-        local VC_LTL_Root = find_in_file() or find_in_reg()
-        if #VC_LTL_Root==0 then
+        local YY_Thunks_Root = find_in_file() or find_in_reg()
+        if #YY_Thunks_Root == 0 then
             return
         end
+
         local WindowsTargetPlatformMinVersion = "5.1.2600.0"
-        cprint("${color.warning}VC-LTL Path : %s", VC_LTL_Root)
+        
+        cprint("${color.warning}YY-Thunks Path : %s", YY_Thunks_Root)
         cprint("${color.warning}WindowsTargetPlatformMinVersion : %s", WindowsTargetPlatformMinVersion)
+        
         import("core.tool.toolchain")
         local msvc = toolchain.load("msvc")
         local runenvs = msvc:runenvs()
-
-        local includepath = VC_LTL_Root .. [[TargetPlatform\header;]] .. VC_LTL_Root .. [[TargetPlatform\]] .. WindowsTargetPlatformMinVersion..[[\header;]]
-
-        runenvs.INCLUDE = includepath .. runenvs.INCLUDE
 
         local arch = target:arch()
         local archpath = "Win32"
@@ -33,10 +32,17 @@ target("VC-LTL-5")
             archpath = arch
         end
         cprint("${color.warning}Platform : %s", archpath)
-        local libpath = VC_LTL_Root .. [[TargetPlatform\]] .. WindowsTargetPlatformMinVersion..[[\lib\]] .. archpath .. ";"
 
+        local libpath = YY_Thunks_Root .. [[Lib\]] .. WindowsTargetPlatformMinVersion .. [[\]] .. archpath .. ";"
         runenvs.LIB = libpath .. runenvs.LIB
 
-        -- print(runenvs.INCLUDE)
-        -- print(runenvs.LIB)
+        if target:kind() == "shared" then
+            target:add("ldflags", "/ENTRY:DllMainCRTStartupForYY_Thunks", {force = true})
+        end
+        
+        if archpath == "x86" then
+            target:add("ldflags", "/SUBSYSTEM:WINDOWS,5.01", {force = true})
+        else
+            target:add("ldflags", "/SUBSYSTEM:WINDOWS,5.02", {force = true})
+        end
     end)
