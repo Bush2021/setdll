5d7a591e24c26e120bb0a378c07e0f7fa2232c46
Bush2021 <79072750+Bush2021@users.noreply.github.com>
2025-05-30 20:52:04 -0400

    chore: add versioning support and release workflow

---
+++ src/version.h
@@ -0,0 +1,15 @@
+#ifndef VERSION_H_
+#define VERSION_H_
+
+#define RELEASE_VER_MAIN 1
+#define RELEASE_VER_SUB 0
+#define RELEASE_VER_FIX 0
+
+#define TOSTRING2(arg) #arg
+#define TOSTRING(arg) TOSTRING2(arg)
+
+#define RELEASE_VER_STR      \
+  TOSTRING(RELEASE_VER_MAIN) \
+  "." TOSTRING(RELEASE_VER_SUB) "." TOSTRING(RELEASE_VER_FIX)
+
+#endif  // VERSION_H_
